8a9006e47d530cd94b75582f00ceff509c0e3e8d
bulutluoz <abbulutluoz@gmail.com>
2024-09-07 16:52:49 +0200

    day02 sonu

---
+++ src/day02_driverMethodlari/C02_IlkTestOtomasyonu.java
@@ -0,0 +1,17 @@
+package day02_driverMethodlari;
+
+public class C02_IlkTestOtomasyonu {
+
+    public static void main(String[] args) {
+
+        //1. testotomasyonu sayfasina gidelim. https://www.testotomasyonu.com/
+        //2. Sayfa basligini(title) yazdirin
+        //3. Sayfa basliginin “Test Otomasyonu” icerdigini test edin
+        //4. Sayfa adresini(url) yazdirin
+        //5. Sayfa url’inin https://testotomasyonu.com/ oldugunu test edin.
+        //6. Sayfa handle degerini yazdirin
+        //7. Sayfa HTML kodlarinda “otomasyon” kelimesi gectigini test edin
+        //8. Sayfayi kapatin.
+
+    }
+}
